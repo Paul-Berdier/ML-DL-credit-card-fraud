0592c6bf5388315619e84bbd51c691753498e8e4
Paul-Berdier <paul.berdier@ynov.com>
2024-12-23 14:20:56 +0100

    Refactor visualization script to improve output handling.
    Moved `plt.show()` to ensure plots are displayed after saving. This prevents potential issues with ordering and enhances clarity in the visualization workflow. Deleted unused V1_V2.png file.

--- utils/visualisation_V1_V2.py
+++ utils/visualisation_V1_V2.py
@@ -6,9 +6,10 @@ def visualisation_V1_V2(input_data, output_image):
     # Visualisation des composantes principales V1 et V2
     sns.scatterplot(data=input_data, x='V1', y='V2', hue='Class', alpha=0.5, palette={0: 'blue', 1: 'red'})
     plt.title('Distribution des transactions sur V1 et V2')
-    plt.show()
     if os.path.exists(output_image):
         print(f"Le fichier '{output_image}' existe déjà.")
     else:
         print(f"Graphique sauvegardé sous : {output_image}")
         plt.savefig(output_image)
+
+    plt.show()
